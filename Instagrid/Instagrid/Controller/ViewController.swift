e42004a71d9c8a8673d6599eeb2c35789ca2b9a1
rudy_zev <rudygeoffray@hotmail.fr>
2020-06-13 17:51:34 +0200

    ajout img base du projet, plus organisation fichier

---
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Rudy on 13/06/2020.
+//  Copyright © 2020 ruzev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
